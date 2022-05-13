df757762c1de14120b8c8f13b86ba7e2b7527b86
Vera K <faith.veyu0@gmail.com>
2022-05-13 16:07:35 +0500

    Lesson 1 commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField('Имя пользователя', unique=True, max_length=64)
+    first_name = models.CharField('Имя', max_length=64)
+    last_name = models.CharField('Фамилия', max_length=64)
+    email = models.EmailField('Почта', unique=True)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from users.models import User
+from users.serializers import UserModelSerializer
 
-# Create your views here.
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
